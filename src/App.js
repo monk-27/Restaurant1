146662b083f3f58e31930ed48e231f185d789cf2
shashi bhushan jha <exquisiteshashi@gmail.com>
2021-07-31 18:27:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// //import React from 'react'
 
-function App() {
+// const App = () => {
+//   return (
+//     <>
+//       <MyName/>
+//       <h1 className = "myclassname">Hello World</h1>
+//       <p>This is my second react project.</p>
+      
+//     </>
+//   );
+// };
+// const MyName = () =>{
+//   return(
+//     <h1>Shashi Bhushan Jha</h1>
+//   )
+// };
+
+// export default App
+import React from 'react'
+import Restaurant from './component/Basics/Restaurant';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Restaurant/>
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/component/menuApi.js
@@ -0,0 +1,66 @@
+const Menu = [
+  {
+    id: 1,
+    image:"images/omelette.jpg",
+    name: "Bread omelette",
+    category: "Breakfast",
+    price: 80,
+    description:
+      "an omelette or omelet is a dish made from beaten eggs, fried with butter or oil in a frying pan (without stirring as in scrambled egg). It is quite common for the omelette to be folded around fillings such as chives, vegetables, mushrooms, meat (often ham or bacon), or some combination of the above. Whole eggs or egg whites are often beaten with a small amount of milk, cream, or water."
+    },
+{
+    id: 2,
+    image:"images/coffee.jpg",
+    name: "Coffee",
+    category: "Breakfast",
+    price: 30,
+    description:
+      "Buy  it and have and have a taste of energetic coffee"
+  },
+  {
+    id: 3,
+    image: "images/lunch.jpg",
+    name: "Veg thali",
+    category: "Lunch",
+    price: 400,
+    description:
+      "we have some most exciting lunch dishes for you come and taste it"
+  },
+  {
+    id: 4,
+    image:"images/momos.jpg",
+    name: "momos",
+    category: "Evening",
+    price: 80,
+    description:
+      "we have some really delicious and tasty momos"
+  },
+  {
+    id: 5,
+    image:"images/nonveg.jpg",
+    name: "Non Veg thali",
+    category: "Dinner",
+    price: 400,
+    description:
+      "Non veg thali is our speciality come and taste it."
+  },
+  {
+    id: 6,
+    image:"images/veg.jpg",
+    name: "Veg thali",
+    category: "Dinner",
+    price: 300,
+    description:
+      "we some best veg thaaliz.",
+  },
+  {
+    id: 7,
+    image:"images/butterchicken.jpg",
+    name: "Butter chicken",
+    category: "Speciality",
+    price: 280,
+    description:
+      "come and enjoy mughalayi butter chicken",
+  },
+];
+export default Menu;
